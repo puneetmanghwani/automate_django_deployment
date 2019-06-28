346f4478c138d4b7ac079106674d721822ae0ec5
puneetmanghwani <punitkumar196@gmail.com>
2019-06-28 17:22:36 +0530

    automated django deployment

---
+++ Git_Project/Git_Project/urls.py
@@ -0,0 +1,28 @@
+"""Git_Project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from git_app import views
+
+# maintain the track of url with associated views-call
+urlpatterns = [
+    path('', views.userpage.as_view(), name='index'),               # move to userpage when user comes to main url
+    path('admin/', admin.site.urls),                                # admin page
+    path('user_graph/<str:username>', views.new_repopage.as_view(), name='new_repopage'),       # dashboard page with username as argument
+    path('fullgraph/', views.fullgraph.as_view(), name='fullgraph'),                            # fullgraph page
+    
+]
